ef4fb6595170cbb18723ea90b35434c0e7ff5939
madina875 <evaevasmq@gmail.com>
2025-09-09 13:21:58 +0500

    hmw

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,9 +1,36 @@
+import {
+  Facebook,
+  Instagram,
+  MessageCircleHeart,
+  Twitter,
+  Youtube,
+} from "lucide-react";
 import React from "react";
 
 const Footer = () => {
   return (
     <footer className="bg-[#A5C926] h-[209px] w-[100%]">
-      <div className="container h-[209px] w-[100%]"></div>
+      <div className="container h-[209px] w-[100%] text-white pt-[40px]">
+        <div className="flex justify-between mb-[50px]">
+          <p className="text-2xl">ToyStore</p>
+          <ul className="flex gap-[20px]">
+            <li>Home</li>
+            <li>Catalog</li>
+            <li>Delivery</li>
+            <li>About</li>
+            <li>Contacts</li>
+          </ul>
+          <div className="flex gap-[20px]">
+            <Twitter /> <Facebook /> <Instagram /> <MessageCircleHeart />
+            <Youtube />
+          </div>
+        </div>
+        <hr className="h-[.5px]" />
+        <div className="flex justify-between pt-[40px]">
+          <p>Created with love by Elastic Themes</p>
+          <p>Powered By Webflow Style Guide Licensing</p>
+        </div>
+      </div>
     </footer>
   );
 };
