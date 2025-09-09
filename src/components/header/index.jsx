ba00aa71f204b4023a5710531de6dd891dd5ac0b
madina875 <evaevasmq@gmail.com>
2025-09-09 07:42:24 +0500

    hmw

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,7 +1,29 @@
+import { ShoppingCart } from "lucide-react";
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="h-[72px] place-items-center">
+      <div className="container flex justify-between place-items-center px-[10px]">
+        <div className="flex place-items-center gap-[50px]">
+          <div className="text-1xl text-2xl">ToyStore</div>
+          <ul className="flex gap-[18px] place-items-center w-[100%] h-[100%]">
+            <li>Catalog</li>
+            <li>Delivery</li>
+            <li>About</li>
+            <li>Contacts</li>
+          </ul>
+        </div>
+        <div className="flex place-items-center gap-[15px]">
+          <p>Cart</p>
+          <ShoppingCart />
+          <button className="rounded-full bg-[#A5C926] text-center w-[20px] h-[20px]">
+            0
+          </button>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import heroImage from "../../assets/herochilds.png";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section
+      className="h-[640px] w-[100%] bg-cover bg-center flex items-center justify-center"
+      style={{ backgroundImage: `url(${heroImage})` }}
+    >
+      <div className="w-[550px] bg-white rounded-[16px] flex flex-col items-center gap-[5px] justify-center h-[300px]">
+        <p className="text-[#A5C926]">Say Hello to ToyStore!</p>
+        <p className="text-[45px] w-[90%] text-center">
+          Free Ecommerce Template for Webflow
+        </p>
+        <button className="w-[150px] h-[40px] bg-[#A5C926] rounded-[24px] text-white">
+          Open catalog
+        </button>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
